30cafd18ead08c0b024fa3320c2c0b27b73b5795
ZacHolme7 <zacholme@gmail.com>
2024-06-12 16:00:59 -0500

    increment and getcount simulation!

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,19 @@
 use alloy_network::EthereumSigner;
 use alloy_node_bindings::Anvil;
+use alloy_primitives::{address, U256};
 use alloy_provider::Provider;
 use alloy_provider::ProviderBuilder;
 use alloy_signer_wallet::LocalWallet;
-use alloy_primitives::U256;
 use alloy_sol_types::{sol, SolCall};
 use anyhow::Result;
 use foundry_evm::fork::{BlockchainDb, BlockchainDbMeta, SharedBackend};
+use revm::interpreter::primitives::{keccak256, AccountInfo, Bytecode, Bytes, TransactTo};
 use revm::{db::CacheDB, Evm};
-use revm::interpreter::primitives::{AccountInfo, Bytecode, Bytes, keccak256};
+use revm_primitives::{ExecutionResult, Output};
 use std::collections::BTreeSet;
 
-
 sol!(
+    #[derive(Debug)]
     #[sol(rpc, bytecode = "6080604052348015600e575f80fd5b506101778061001c5f395ff3fe608060405234801561000f575f80fd5b506004361061003f575f3560e01c806306661abd14610043578063a87d942c14610061578063d09de08a1461007f575b5f80fd5b61004b610089565b60405161005891906100c8565b60405180910390f35b61006961008e565b60405161007691906100c8565b60405180910390f35b610087610096565b005b5f5481565b5f8054905090565b60015f808282546100a7919061010e565b92505081905550565b5f819050919050565b6100c2816100b0565b82525050565b5f6020820190506100db5f8301846100b9565b92915050565b7f4e487b71000000000000000000000000000000000000000000000000000000005f52601160045260245ffd5b5f610118826100b0565b9150610123836100b0565b925082820190508082111561013b5761013a6100e1565b5b9291505056fea2646970667358221220dc61928b75c9f79e8b82b86e93a996b4da03268809cca775e9130181f9b398eb64736f6c634300081a0033")]
     contract Counter {
         uint256 public count;
@@ -30,9 +31,7 @@ sol!(
 #[tokio::main]
 async fn main() -> Result<()> {
     // setup provdier to forked anvil
-    let anvil = Anvil::new()
-        .fork("https://eth.merkle.io")
-        .try_spawn()?;
+    let anvil = Anvil::new().fork("https://eth.merkle.io").try_spawn()?;
 
     let signer: LocalWallet = anvil.keys()[0].clone().into();
 
@@ -74,84 +73,37 @@ async fn main() -> Result<()> {
 
     let mut_db = evm.db_mut();
 
-    // 10 ether
-    let ten_eth = U256::from(10).checked_mul(U256::from(10).pow(U256::from(18))).unwrap();
+    // setup user account and insert into database
+    let user = address!("18B06aaF27d44B756FCF16Ca20C1f183EB49111f"); // 10 ether
+    let ten_eth = U256::from(10)
+        .checked_mul(U256::from(10).pow(U256::from(18)))
+        .unwrap();
+    let user_account_info =
+        AccountInfo::new(ten_eth, 0, keccak256(Bytes::new()), Bytecode::default());
+    mut_db.insert_account_info(user, user_account_info);
 
-    // set a user account
-    let user = AccountInfo::new(ten_eth, 0, keccak256(Bytes::new()), Bytecode::default());
-
-
-
-    println!("{:?}", evm.db());
-
-
-
-
-
-
-
-
-
+    // call increment
     let increment_call_encode = Counter::incrementCall::new(()).abi_encode();
+    evm.tx_mut().caller = address!("0000000000000000000000000000000000000000");
+    evm.tx_mut().transact_to = TransactTo::Call(*contract.address());
+    evm.tx_mut().data = increment_call_encode.into();
+    evm.transact_commit().unwrap();
 
+    // call getCount
+    let getcount_call_encode = Counter::getCountCall::new(()).abi_encode();
+    evm.tx_mut().data = getcount_call_encode.into();
+    let ref_tx = evm.transact().unwrap();
+    let result = ref_tx.result;
 
-
-
-
-
-    //let increment_call_decode = incrementCall::abi_decode_return(data, true)?;
-
-    /*
-
-    let db = CacheDB::new(EmptyDB::default());
-    let mut evm = Evm::builder().with_db(db).build();
-
-
-    let user = address!("E2b5A9c1e325511a227EF527af38c3A7B65AFA1d");
-    let usdt = address!("dAC17F958D2ee523a2206206994597C13D831ec7");
-
-
-
-    //let calldata = balanceOfCall { owner: user }.abi_encode();
-    let calldata = balanceOfCall::new((user,)).abi_encode();
-    // clones the current dst of tx, evm.tx() get ref to current tx setup, transact_to is the dst,
-    // clone will clone it
-    let original = evm.tx().transact_to.clone();
-
-    // now we can rewrite over it
-    // set that we are doing a call to the usdt address
-    evm.tx_mut().transact_to = TransactTo::Call(usdt);
-    evm.tx_mut().data = calldata.into();
-
-    let result = match evm.transact() {
-        Ok(result) => result,
-        Err(e) => return Err(anyhow!("EVM ref call failed: {e:?}")),
-    };
-
-    let tx_result = match result.result {
+    let value = match result {
         ExecutionResult::Success {
-            gas_used,
-            gas_refunded,
-            output,
-            logs,
+            output: Output::Call(value),
             ..
-        } => match output {
-            Output::Call(o) => TxResult {
-                output: o,
-                logs: Some(logs),
-                gas_used,
-                gas_refunded,
-            },
-            _ => panic!("should not reach here")
-        }
-        _ => panic!("should not reach here")
+        } => value,
+        _result => panic!("it failed"),
     };
 
-
-    let ret = balanceOfCall::abi_decode_returns(&tx_result.output, true)?;
-
-
-    println!("{:?}", tx_result);
-    */
+    let output = Counter::getCountCall::abi_decode_returns(&value, false)?;
+    println!("Output: {:?}", output);
     Ok(())
 }
