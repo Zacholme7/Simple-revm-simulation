14f2279a7a7c6ef7b935dbc33cfbf4262ba2f798
ZacHolme7 <zacholme@gmail.com>
2024-09-25 18:49:17 -0500

    updated example

--- src/main.rs
+++ src/main.rs
@@ -1,111 +1,72 @@
-use alloy_network::EthereumSigner;
-use alloy_node_bindings::Anvil;
-use alloy_primitives::{address, U256};
-use alloy_provider::Provider;
-use alloy_provider::ProviderBuilder;
-use alloy_signer_wallet::LocalWallet;
-use alloy_sol_types::{sol, SolCall};
+use alloy::primitives::{address, U256};
+use alloy::sol;
+use alloy::sol_types::{SolCall, SolValue};
 use anyhow::Result;
-use foundry_evm::fork::{BlockchainDb, BlockchainDbMeta, SharedBackend};
-use revm::interpreter::primitives::{keccak256, AccountInfo, Bytecode, Bytes, TransactTo};
-use revm::{db::CacheDB, Evm};
-use revm_primitives::{ExecutionResult, Output};
-use std::collections::BTreeSet;
+use revm::db::{CacheDB, EmptyDB};
+use revm::primitives::{AccountInfo, Bytecode, ExecutionResult, Output, TransactTo};
+use revm::Evm;
 
-sol!(
-    #[derive(Debug)]
-    #[sol(rpc, bytecode = "6080604052348015600e575f80fd5b506101778061001c5f395ff3fe608060405234801561000f575f80fd5b506004361061003f575f3560e01c806306661abd14610043578063a87d942c14610061578063d09de08a1461007f575b5f80fd5b61004b610089565b60405161005891906100c8565b60405180910390f35b61006961008e565b60405161007691906100c8565b60405180910390f35b610087610096565b005b5f5481565b5f8054905090565b60015f808282546100a7919061010e565b92505081905550565b5f819050919050565b6100c2816100b0565b82525050565b5f6020820190506100db5f8301846100b9565b92915050565b7f4e487b71000000000000000000000000000000000000000000000000000000005f52601160045260245ffd5b5f610118826100b0565b9150610123836100b0565b925082820190508082111561013b5761013a6100e1565b5b9291505056fea2646970667358221220dc61928b75c9f79e8b82b86e93a996b4da03268809cca775e9130181f9b398eb64736f6c634300081a0033")]
-    contract Counter {
-        uint256 public count;
+// Generate our contract interface with the sol! macro
+sol!(Counter, "Counter.json");
 
-        function increment() public {
-            count += 1;
-        }
+fn main() -> Result<()> {
+    // Dummy addresses we will use
+    let counter_address = address!("A5C381211A406b48A073E954e6949B0D49506bc0");
+    let caller = address!("0000000000000000000000000000000000000001");
 
-        function getCount() public view returns (uint256) {
-            return count;
-        }
+    // Create the db to hold all EVM related state. This is analogous to the DB on the blockchain.
+    // Any state will be stored here
+    let mut db = CacheDB::new(EmptyDB::new());
+
+    // insert our contract into the DB. In a way, this is "deploying" the contract to the chain.
+    let counter_bytecode = Bytecode::new_raw(Counter::DEPLOYED_BYTECODE.clone());
+    let counter_bytecode_hash = counter_bytecode.hash_slow();
+    let counter_account = AccountInfo {
+        balance: U256::ZERO,
+        nonce: 0_u64,
+        code: Some(counter_bytecode),
+        code_hash: counter_bytecode_hash,
+    };
+    db.insert_account_info(counter_address, counter_account);
+
+    // Encode our increment call
+    let increment_calldata = Counter::incrementCall {}.abi_encode();
+
+    // Construct the evm instance, this is what we use to execute the transaction
+    let mut evm = Evm::builder()
+        .with_db(db)
+        .modify_tx_env(|tx| {
+            tx.caller = caller;
+            tx.transact_to = TransactTo::Call(counter_address);
+            tx.data = increment_calldata.into();
+            tx.value = U256::ZERO;
+        })
+        .build();
+
+    // transact and commit this transaction to the database!
+    if let Err(e) = evm.transact_commit() {
+        panic!("Increment call failed: {:?}", e);
     }
-);
 
-#[tokio::main]
-async fn main() -> Result<()> {
-    // setup provider to forked anvil
-    let anvil = Anvil::new().fork("https://eth.merkle.io").try_spawn()?;
+    // Following the same procedure above, we can now call the getCount function and we should see
+    // an incremented count. All we have to do it modify the calldata as we just want to call a
+    // different function on the contract
+    let getcount_calldata = Counter::getCountCall {}.abi_encode();
+    evm.tx_mut().data = getcount_calldata.into();
 
-    // signer to deploy the contract
-    let signer: LocalWallet = anvil.keys()[0].clone().into();
-
-    // provider communicating with anvil
-    let provider = ProviderBuilder::new()
-        .with_recommended_fillers()
-        .network::<alloy_network::AnyNetwork>()
-        .signer(EthereumSigner::from(signer))
-        .on_builtin(&anvil.endpoint())
-        .await?;
-
-    // deploy the contract
-    let contract = Counter::deploy(&provider).await?;
-    println!("Deployed contract at {}", contract.address());
-
-    let block_number = provider.get_block_number().await?;
-
-    // setup shared backend
-    let shared_backend = SharedBackend::spawn_backend_thread(
-        provider.clone(),
-        BlockchainDb::new(
-            BlockchainDbMeta {
-                cfg_env: Default::default(),
-                block_env: Default::default(),
-                hosts: BTreeSet::from(["".to_string()]),
-            },
-            None,
-        ),
-        Some(block_number.into()),
-    );
-
-    let db = CacheDB::new(shared_backend);
-    let mut evm = Evm::builder().with_db(db).build();
-
-    // modify the env
-    evm.cfg_mut().limit_contract_code_size = Some(0x100000);
-    evm.cfg_mut().disable_block_gas_limit = true;
-    evm.cfg_mut().disable_base_fee = true;
-    evm.block_mut().number = U256::from(block_number + 1);
-
-    let mut_db = evm.db_mut();
-
-    // setup user account and insert into database
-    let user = address!("18B06aaF27d44B756FCF16Ca20C1f183EB49111f");
-    let ten_eth = U256::from(10)
-        .checked_mul(U256::from(10).pow(U256::from(18)))
-        .unwrap();
-    let user_account_info =
-        AccountInfo::new(ten_eth, 0, keccak256(Bytes::new()), Bytecode::default());
-    mut_db.insert_account_info(user, user_account_info);
-
-    // call increment
-    let increment_call_encode = Counter::incrementCall::new(()).abi_encode();
-    evm.tx_mut().caller = address!("0000000000000000000000000000000000000000");
-    evm.tx_mut().transact_to = TransactTo::Call(*contract.address());
-    evm.tx_mut().data = increment_call_encode.into();
-    evm.transact_commit().unwrap();
-
-    // modify transaction and call getCount
-    let getcount_call_encode = Counter::getCountCall::new(()).abi_encode();
-    evm.tx_mut().data = getcount_call_encode.into();
     let ref_tx = evm.transact().unwrap();
     let result = ref_tx.result;
 
-    let value = match result {
+    match result {
         ExecutionResult::Success {
             output: Output::Call(value),
             ..
-        } => value,
-        _result => panic!("it failed"),
+        } => {
+            let count = <U256>::abi_decode(&value, false).unwrap();
+            println!("Count is {}!", count);
+        }
+        _result => panic!("Get count call failed!"),
     };
 
-    let output = Counter::getCountCall::abi_decode_returns(&value, false)?;
-    println!("Output after incrementing: {:?}", output);
     Ok(())
 }
